57ef12a6703f1927ad82bd6c9c4eeb4520cf6ebe
Yuri <32842102+TicTacT0e@users.noreply.github.com>
2017-12-11 20:01:18 +0300

    up

---
+++ ScoreViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApplication1
+{
+    class ScoreViewModel : INotifyPropertyChanged
+    {
+        private Score score;
+        public ScoreViewModel(Score s)
+        {
+            score = s;
+        }
+        public int Number
+        {
+            get { return score.Number; }
+            set
+            {
+                score.Number = value;
+                OnPropertyChanged("Number");
+            }
+        }
+        public string Type
+        {
+            get { return score.Type; }
+            set
+            {
+                score.Type = value;
+                OnPropertyChanged("Type");
+            }
+        }
+        public int Balance
+        {
+            get { return score.Balance; }
+            set
+            {
+                score.Balance = value;
+                OnPropertyChanged("Balance");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
