7a08a64e4d46d52d7548cb9e9ae092f6be64bec5
Yuri <32842102+TicTacT0e@users.noreply.github.com>
2018-02-25 00:35:42 +0300

    fixrep

---
+++ Score.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApplication1
+{
+    class Score : INotifyPropertyChanged
+    {
+        private int number;
+        private string type;
+        private int balance;
+        public int Number
+        {
+            get { return number; }
+            set
+            {
+                number = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        public string Type
+        {
+            get { return type; }
+            set
+            {
+                type = value;
+                OnPropertyChanged("Company");
+            }
+        }
+
+        public int Balance
+        {
+            get { return balance; }
+            set
+            {
+                balance = value;
+                OnPropertyChanged("Price");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
